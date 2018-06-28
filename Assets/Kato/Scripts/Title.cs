345300c184e8b8f5b2cd436fe7e4d08de9d19f8e
DESKTOP-28F38VP\user <nkc.sousuke.katou@gmail.com>
2018-06-28 15:13:59 +0900

    StageCreate移動

--- Assets/Kato/Scripts/Title.cs
+++ Assets/Kato/Scripts/Title.cs
@@ -2,17 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Title : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update ()
+public class Title : MonoBehaviour
+{
+    
+    // Use this for initialization
+    void Start()
     {
-        // ワールドのy軸に沿って1秒間に90度回転
-        transform.Rotate(new Vector3(0, 30,0) * Time.deltaTime, Space.World);
+        
     }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(0, 90, 0) * Time.deltaTime, Space.World);
+    }
+
 }
